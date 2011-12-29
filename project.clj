b7e0934137cd726f16324627938ef19a3affa05c
Evan Mezeske <evan@meebo-inc.com>
2011-12-29 16:34:01 -0700

    Omit some unnecessary files from the jar.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject emezeske/closure-library "r1376"
   :description "Google Closure Library"
   :url "http://github.com/emezeske/closure-library"
+  :jar-exclusions [#"alltests\.js" #"all_tests\.html" #"AUTHORS" #"LICENSE" #"README"] 
   :resources-path "upstream")
