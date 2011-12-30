b20510e8cb41bf3feb152edd29388af174f43c1d
Evan Mezeske <evan@meebo-inc.com>
2011-12-29 18:01:30 -0700

    Organize the resources correctly.

--- project.clj
+++ project.clj
@@ -1,5 +1,14 @@
 (defproject emezeske/closure-library "r1376"
   :description "Google Closure Library"
   :url "http://github.com/emezeske/closure-library"
-  :jar-exclusions [#"alltests\.js" #"all_tests\.html" #"AUTHORS" #"LICENSE" #"README"] 
-  :resources-path "upstream")
+  :dev-dependencies [[fs "0.11.1"]]
+  :jar-exclusions [#"closure" #"bin" #"css" #"known_issues"]  
+  :resources-path "resources")
+
+(use '[leiningen.core :only [prepend-tasks]]
+     '[leiningen.pom :only [pom]]
+     '[leiningen.jar :only [jar]]
+     '[build.core :only [copy-resources]])
+
+(prepend-tasks #'pom copy-resources)
+(prepend-tasks #'jar copy-resources)
---
+++ src/build/core.clj
@@ -0,0 +1,12 @@
+(ns build.core
+  (:use
+    [leiningen.compile :only [eval-in-project]])) 
+
+(defn copy-resources [project]
+  (eval-in-project project
+    `(let [~'dest ~(:resources-path project)]
+      (fs/copy-tree "upstream/closure" ~'dest)
+      (fs/copy-tree "upstream/third_party/closure/goog" ~'(fs/join dest "third_party/goog")))
+      nil 
+      nil 
+      '(require 'fs)))
