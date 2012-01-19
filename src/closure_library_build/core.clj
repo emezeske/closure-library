52220e02526cf5e29b6b7c7746116166d7db700b
Evan Mezeske <evan@mezeske.com>
2012-01-18 22:16:27 -0700

    Whitespace changes.

--- src/closure_library_build/core.clj
+++ src/closure_library_build/core.clj
@@ -1,19 +1,19 @@
 (ns closure-library-build.core
   (:use
-    [leiningen.compile :only [eval-in-project]])) 
+    [leiningen.compile :only [eval-in-project]]))
 
 (defn copy-resources [project]
   (eval-in-project project
     `(let [dest# ~(:resources-path project)]
       (when (fs/exists? dest#)
-	(fs/deltree dest#) 
+        (fs/deltree dest#)
         (fs/mkdir dest#))
       (fs/copy-tree "upstream/third_party/closure/goog" dest#)
       (fs/copy-tree "upstream/closure/goog" dest#)
       (let [deps# (slurp (fs/join dest# "goog/deps.js"))
             rep# (.replaceAll deps# "\\.\\./\\.\\./third_party/closure/goog/" "")]
         (spit (fs/join dest# "goog/deps.orig") deps#)
-	(spit (fs/join dest# "goog/deps.js") rep#)))
-      nil 
-      nil 
+        (spit (fs/join dest# "goog/deps.js") rep#)))
+      nil
+      nil
       '(require 'fs)))
